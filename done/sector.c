3464f65e0153d0595de222c51f784e39f3ccfefc
Mounir Amrani <mounir.amrani@epfl.ch>
2017-03-25 20:57:46 +0100

    Corrected sector.c and Changed Makefile, sector.c now compiles

--- done/sector.c
+++ done/sector.c
@@ -9,7 +9,7 @@ int sector_read(FILE *f, uint32_t sector, void *data)
 	M_REQUIRE_NON_NULL(data); // return error message if data == NULL
 	
 	fseek(f, SECTOR_SIZE * sector, SEEK_SET); // move cursor SECTOR_SIZE * sector bytes from the beginning of the disk (sector start point)
-	int elemRead = fread(data, sizeof(u8int), SECTOR_SIZE, f); // read SECTOR_SIZE bytes from f to data
+	int elemRead = fread(data, sizeof(uint8_t), SECTOR_SIZE, f); // read SECTOR_SIZE bytes from f to data
 	
 	if(elemRead == SECTOR_SIZE) // no error
 	{
