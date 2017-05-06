1710c65d79e4f30727cf0d5de67b6ffbb7bec950
Mounir Amrani <mounir.amrani@epfl.ch>
2017-05-07 01:02:48 +0200

    Corrected bm_print

--- done/bmblock.c
+++ done/bmblock.c
@@ -120,8 +120,9 @@ void bm_print(struct bmblock_array *bmblock_array)
         for(uint64_t i = 0; i < bmblock_array->length; i++) { //iterate on 64 bits blocs
             printf("%lu:",i);
             
-            for(uint64_t x = i * BITS_PER_VECTOR + min; x < max; x++) { //iterate the blocs bits
-                printf("%s%u", ((x - min) % 8 == 0) ? " " : "", bm_get(bmblock_array, x)); //get bit and print it
+            for(int j = 0; j < BITS_PER_VECTOR; j++) { //iterate the blocs bits
+				uint64_t x = i*BITS_PER_VECTOR + j + min; // element number
+                printf("%s%u", (j % 8 == 0) ? " " : "", bm_get(bmblock_array, x)); //get bit and print it
             }
             
             printf("\n");
