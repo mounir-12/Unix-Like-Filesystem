ee60a0e879ee5286618e537bf89e0f2061388cd4
maximiliengroh <maximilien.grh@epfl.ch>
2017-05-06 21:02:34 +0200

    completed struct bmblock_array and created bmblock.c

---
+++ done/bmblock.c
@@ -0,0 +1,32 @@
+#include <stdint.h>
+#include "inode.h"
+
+struct bmblock_array *bm_alloc(uint64_t min, uint64_t max)
+{
+	
+}
+
+int bm_get(struct bmblock_array *bmblock_array, uint64_t x)
+{
+	
+}
+
+void bm_set(struct bmblock_array *bmblock_array, uint64_t x)
+{
+	
+}
+
+void bm_clear(struct bmblock_array *bmblock_array, uint64_t x)
+{
+	
+}
+
+int bm_find_next(struct bmblock_array *bmblock_array)
+{
+	
+}
+
+void bm_print(struct bmblock_array *bmblock_array)
+{
+	
+}
