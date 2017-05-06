f29251440ebc3d7d48e7b968e5d21b60023252b1
maximiliengroh <maximilien.grh@epfl.ch>
2017-05-06 22:51:46 +0200

    added bm_set()

--- done/bmblock.c
+++ done/bmblock.c
@@ -4,37 +4,38 @@
 
 struct bmblock_array *bm_alloc(uint64_t min, uint64_t max)
 {
-	if(min > max) {
+    if(min > max) {
         return NULL;
     }
     size_t length = max - min + 1; // number of elements
     size_t nb = length/64 + (length % 64 == 0 ? 0 : 1); // number of uint64_t to allocate to cover all elements
     const size_t N_MAX = (SIZE_MAX - sizeof(struct bmblock_array)) / sizeof(uint64_t) +1; // max number of uint64_t to allocate
-    
-    if(nb <= N_MAX)
-    {
-		struct bmblock_array *a = malloc(sizeof(struct bmblock_array) + (nb-1)*sizeof(uint64_t) );
-		if(a != NULL)
-		{
-			    a->length = nb;
-				a->cursor = 0;
-				a->min = min;
-				a->max = max;
-				return a;
-		}
-	}
-	
-	return NULL;
+
+    if(nb <= N_MAX) {
+        struct bmblock_array *a = malloc(sizeof(struct bmblock_array) + (nb-1)*sizeof(uint64_t) );
+        if(a != NULL) {
+            a->length = nb;
+            a->cursor = 0;
+            a->min = min;
+            a->max = max;
+            return a;
+        }
+    }
+
+    return NULL;
 }
 
 int bm_get(struct bmblock_array *bmblock_array, uint64_t x)
 {
-	return 0;
+    return 0;
 }
 
 void bm_set(struct bmblock_array *bmblock_array, uint64_t x)
 {
-
+	M_REQUIRE_NON_NULL(bmblock_array);
+	if(x >= bmblock_array->min && x <= bmblock_array->max){
+		bmblock_array->bm[x] =  1;
+	}
 }
 
 void bm_clear(struct bmblock_array *bmblock_array, uint64_t x)
@@ -44,7 +45,7 @@ void bm_clear(struct bmblock_array *bmblock_array, uint64_t x)
 
 int bm_find_next(struct bmblock_array *bmblock_array)
 {
-	return 0;
+    return 0;
 }
 
 void bm_print(struct bmblock_array *bmblock_array)
