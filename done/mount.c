3d794a45964d4c46ccdeaaa94cbc31f6c8f2044c
Mounir Amrani <mounir.amrani@epfl.ch>
2017-05-13 21:07:47 +0200

    Changed implementation of mountv6 in mount.c

--- done/mount.c
+++ done/mount.c
@@ -13,16 +13,7 @@ int mountv6(const char *filename, struct unix_filesystem *u)
     M_REQUIRE_NON_NULL(u);
     //init u
     memset(u, 0, sizeof(*u));
-    
-    uint64_t min_fbm = 0;
-    uint64_t max_fbm = 0;
-    u->fbm = bm_alloc(min, max);
-    uint64_t min_ibm = 0;
-    uint64_t max_ibm = 0;
-    u->ibm = bm_alloc(min, max);
-    
-    fill_fbm(u);
-    fill_ibm(u);
+
 
     u->f = fopen(filename,"rw"); // open file in u->f in binary read mode
     if(u->f == NULL) { // open error
@@ -36,7 +27,21 @@ int mountv6(const char *filename, struct unix_filesystem *u)
         } else if(error) { // read error
             return error;
         } else { // correctly mounted
-            return sector_read(u->f,SUPERBLOCK_SECTOR,&(u->s)); // read and return the returned value: 0 if success, error otherwise
+            int error = sector_read(u->f,SUPERBLOCK_SECTOR,&(u->s)); // read and return the returned value: 0 if success, error otherwise
+            if(error) { // error occured
+                return error; // propagate error
+            }
+            uint64_t min_fbm = (u->s).s_block_start; // data sectors start
+            uint64_t max_fbm = (u->s).s_fsize; // data sectors end
+            u->fbm = bm_alloc(min_fbm, max_fbm); // allocate data sectors bitmaps
+            
+            uint64_t min_ibm = (u->s).s_inode_start; // inode sectors start
+            uint64_t max_ibm = (u->s).s_inode_start + (u->s).s_isize-1; // inode sectors end
+            u->ibm = bm_alloc(min_ibm, max_ibm); // allocate inode sectors bitmaps
+
+            fill_fbm(u);
+            fill_ibm(u);
+            return 0;
         }
     }
 }
@@ -102,6 +107,6 @@ void fill_ibm(struct unix_filesystem *u)
 
 void fill_fbm(struct unix_filesystem *u)
 {
-    
-    
+
+
 }
