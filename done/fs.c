7eea522c6a006585c271d4fcacf91425382cb26c
Mounir Amrani <mounir.amrani@epfl.ch>
2017-05-14 10:50:15 +0200

    Removed commented code in fs.c

--- done/fs.c
+++ done/fs.c
@@ -154,27 +154,13 @@ static int fs_read(const char *path, char *buf, size_t size, off_t offset,
     if(error) { // error found
         return 0; // return 0 to signal error (no byte read to buf)
     }
-    /*
-    int read = 0; // number of read bytes
-    unsigned char data[SECTOR_SIZE]; // data of the sector to be read
-    read = filev6_readblock(&fv6, data); // read sector
-    if(read <=0) // error occured or nothing read
-    {
-		return 0; // return 0
-	}
-	
-	memcpy(buf, data, read); // copy read data to buffer
-	
-	return read; // return number of read bytes
-	
-    */
-    
+
     size_t blocksToRead = size / SECTOR_SIZE; // in order to always read block by block
     size_t bytesToRead = blocksToRead * SECTOR_SIZE; // number of bytes to read
     unsigned char data[bytesToRead]; // data of the file
 	
 	size_t dataOffset = 0; // offset for data, also total number of read bytes
-	int read = 1; // read bytes in one read
+	int read = 1; // number of read bytes in one read
 
     while(read > 0 && dataOffset < bytesToRead) // loop while can still read and didn't read max size
     {
