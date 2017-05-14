9989a14cdb7103ca2e731d6e0c6a89496536ab54
Mounir Amrani <mounir.amrani@epfl.ch>
2017-05-14 13:47:50 +0200

    Changed test-write

--- done/test-write.c
+++ done/test-write.c
@@ -12,8 +12,12 @@ int test(struct unix_filesystem *u)
 	uint16_t FIL = IALLOC | IFMT; // allocated file
 	struct filev6 fv6;
 	fv6.u = u;
-	fv6.i_number = 3;
+	fv6.i_number = 4;
 	fv6.offset = 0;
-	filev6_create(u, DIR, &fv6);
+	int error = filev6_create(u, DIR, &fv6);
+	if(error)
+	{
+		debug_print("%s\n", ERR_MESSAGES[error - ERR_FIRST]);
+	}
 	return inode_scan_print(u);
 }
