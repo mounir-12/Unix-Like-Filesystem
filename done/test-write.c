628a5afbf7c2c9b7660f3cb0cf710ffb68e57cad
Mounir Amrani <mounir.amrani@epfl.ch>
2017-05-14 14:52:46 +0200

    chnaged FIL variable in test-write

--- done/test-write.c
+++ done/test-write.c
@@ -9,12 +9,13 @@
 int test(struct unix_filesystem *u)
 {
 	uint16_t DIR = IALLOC | IFDIR; // allocated directory
-	uint16_t FIL = IALLOC | IFMT; // allocated file
+	uint16_t FIL = IALLOC; // allocated file
+	
 	struct filev6 fv6;
 	fv6.u = u;
 	fv6.i_number = 3;
 	fv6.offset = 0;
-	int error = filev6_create(u, DIR, &fv6);
+	int error = filev6_create(u, FIL, &fv6);
 	if(error)
 	{
 		debug_print("%s\n", ERR_MESSAGES[error - ERR_FIRST]);
